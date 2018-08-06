721b8bd222eaac276e45e1de6e573b5a0fc6dd80
JungGuIk <rndlr96@gmail.com>
2018-08-06 00:01:34 -0700

    [ADD] Upload buddy pairs goroutine version solution and basic solution (5kyu)

--- Codewars/buddy_pairs/goroutine_version.go
+++ Codewars/buddy_pairs/goroutine_version.go
@@ -4,17 +4,22 @@ import "math"
 
 // the return is `nil` if there is no buddy pair
 func Buddy(start, limit int) []int {
+	result := make(chan []int)
+	var arr []int
 
-	for i := start; i <= limit; i++ {
-		pair1 := properSum(i) - 1
-		pair2 := properSum(pair1) - 1
+	for j := start; j <= limit; j++ {
+		go func(j int) {
+			pair1 := properSum(j) - 1
+			pair2 := properSum(pair1) - 1
 
-		if i < pair1 && pair2 == i {
-			return []int{pair2, pair1}
-		}
+			if pair2 == j {
+				result <- []int{pair2, pair1}
+			}
+		}(j)
 	}
 
-	return nil
+	arr = <-result
+	return arr
 	// your code
 }
 
@@ -22,7 +27,7 @@ func properSum(n int) int {
 	if n <= 0 {
 		return 0
 	}
-	sum := 1
+	var sum int
 
 	l := int(math.Sqrt(float64(n)))
 	for i := 2; i <= l; i++ {
