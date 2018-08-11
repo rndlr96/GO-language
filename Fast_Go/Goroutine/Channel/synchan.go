7791106c8878cc67254c19e4a6624338036bafbd
JungGuIk <rndlr96@gmail.com>
2018-08-10 20:40:50 -0700

    [MOD] Rearrange (sort through kyu)

--- Fast_Go/Goroutine/Channel/synchan.go
+++ Fast_Go/Goroutine/Channel/synchan.go
@@ -1,24 +1,24 @@
 package package_name
 
 import (
-    "fmt"
-//    "time"
+	"fmt"
+	//    "time"
 )
 
 func main() {
-    done := make(chan bool)
-    count := 3
+	done := make(chan bool)
+	count := 3
 
-    go func() {
-        for i := 0 ; i < count ; i++ {
-            fmt.Println("Goroutine : ", i)
-            done <- true // waiting until main get done value
-            //time.Sleep(1 * time.Second) // wait 1 Second
-        }
-    }()
+	go func() {
+		for i := 0; i < count; i++ {
+			fmt.Println("Goroutine : ", i)
+			done <- true // waiting until main get done value
+			//time.Sleep(1 * time.Second) // wait 1 Second
+		}
+	}()
 
-    for i := 0 ; i < count ; i++ {
-        <-done // waiting until value to come into the chan
-        fmt.Println("Main : ", i)
-    }
+	for i := 0; i < count; i++ {
+		<-done // waiting until value to come into the chan
+		fmt.Println("Main : ", i)
+	}
 }
