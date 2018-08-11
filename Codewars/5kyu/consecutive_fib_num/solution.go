12a12c40e2f14e183c0a082e06d3b9dbb7590b52
JungGuIk <rndlr96@gmail.com>
2018-08-11 08:13:20 -0700

    [MOD] Modify for complexity

--- Codewars/5kyu/consecutive_fib_num/solution.go
+++ Codewars/5kyu/consecutive_fib_num/solution.go
@@ -1,28 +1,25 @@
 package kata
 
+import "math"
+
 func ProductFib(prod uint64) [3]uint64 {
 	// your code
 	var result, find1, find2 uint64
 	var tmp int
 
-	for i := 0; result <= prod; i++ {
-		find1 = findresult(i)
-		find2 = findresult(i + 1)
-		result = uint64(find1 * find2)
-		if uint64(find1*find2) == prod {
+	for i := 2; result <= prod; i++ {
+		find1 = ratio(i)
+		find2 = ratio(i + 1)
+		result = find1 * find2
+		if result == prod {
 			return [3]uint64{find1, find2, 1}
 		}
 		tmp = i
 	}
-	return [3]uint64{findresult(tmp), findresult(tmp + 1), 0}
+	return [3]uint64{ratio(tmp), ratio(tmp + 1), 0}
 }
 
-func findresult(n int) uint64 {
-	if n == 0 {
-		return 0
-	} else if n == 1 {
-		return 1
-	} else {
-		return findresult(n-1) + findresult(n-2)
-	}
+func ratio(n int) uint64 {
+	one := math.Pow(((1 + math.Sqrt(5.0)) / 2), float64(n))
+	return uint64(math.Round(one / math.Sqrt(5.0)))
 }
