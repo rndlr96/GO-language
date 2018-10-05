313aae056db438f17dfaabe563fb70263477dc75
Guik <rndlr96@gmail.com>
2018-10-05 22:33:17 +0900

    [ADD] upload codewars 7kyu solution

---
+++ src/Codewars/7kyu/mumbling/solution.go
@@ -0,0 +1,22 @@
+package kata
+
+import (
+	"unicode"
+)
+
+func Accum(s string) string {
+	// your code
+	var input string
+
+	for k, v := range s {
+		for i := 0; i < k+1; i++ {
+			if i == 0 {
+				input += string(unicode.ToUpper(v))
+			} else {
+				input += string(unicode.ToLower(v))
+			}
+		}
+		input += string('-')
+	}
+	return input[:len(input)-1]
+}
