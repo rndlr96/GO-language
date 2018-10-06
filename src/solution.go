3edfd6a67002e336115e86c8c334178602eecb0a
Guik <rndlr96@gmail.com>
2018-10-07 01:28:04 +0900

    [MOD] HowMuch solution modularization

--- src/solution.go
+++ src/solution.go
@@ -11,12 +11,17 @@ func HowMuch(m int, n int) [][3]string {
 
     for i := m; i <= n; i++ {
         if (i - 1) % 9 == 0 && (i - 2) % 7 == 0 {
-            results = append(results, [3]string{
-                "M: " + strconv.Itoa(i),
-                "B: " + strconv.Itoa((i - 2) / 7),
-                "C: " + strconv.Itoa((i - 1) / 9)})
+            results = append(results, MakeString(i, (i-2)/7, (i-1)/9))
         }
     }
 
     return results
+}
+
+func MakeString(int money, int car, int boat) [3]string {
+  return [3]string {
+        "M: " + strconv.Itoa(money),
+        "B: " + strconv.Itoa(car),
+        "C: " + strconv.Itoa(boat)
+  }
 }
