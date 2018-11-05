c9dc4ff7f4d098be56b95bc205cf07209be0606d
Guik <rndlr96@gmail.com>
2018-11-05 19:28:01 +0900

    [ADD] upload binary search tree

---
+++ src/Algorithm/search/binary.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+  "fmt"
+)
+
+type node struct {
+  left *node
+  val int
+  right *node
+}
+
+func insert_tree(root *node, val int) *node {
+  if root == nil {
+    root := new(node)
+    root.val = val
+    root.left, root.right = nil, nil
+  } else {
+    if root.val < val {
+      root.right = insert_tree(root.right, val)
+    } else if root.val > val {
+      root.left = insert_tree(root.left, val)
+    }
+  }
+  return root
+}
+
+func print_tree(root *node) int {
+    if root == nil {
+      return 0
+    } else {
+      fmt.Println(root.val)
+      print_tree(root.left)
+      print_tree(root.right)
+      return 0
+    }
+    return 0
+}
+
+func main(){
+  var root *node
+
+  root = insert_tree(root, 5)
+  root = insert_tree(root, 3)
+
+
+  print_tree(root)
+}
