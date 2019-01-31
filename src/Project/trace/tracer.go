4d9fec3af27f699c870e47dc8db40f85e7e934e4
Guik Marcus Jung <rndlr96@gmail.com>
2019-01-31 16:12:37 +0900

    [ADD] upload trace

---
+++ src/Project/trace/tracer.go
@@ -0,0 +1,23 @@
+package trace
+
+import (
+  "io"
+  "fmt"
+)
+
+type Tracer interface {
+  Trace(...interface{})
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+type tracer struct {
+  out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
+}
